d5899c05b6d0b78e8208067c2ee3baf27881068c
Lakey31 <143187573+Lakey31@users.noreply.github.com>
2024-09-02 02:22:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "image එකක url එකක් මෙතෙන්න දන්න",
-ALIVE_MSG: process.env.ALIVE_MSG || "Ane Hutto"
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imghippo.com/files/pm4fo1725220281.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am Nimeshbot I am alive now!",
 };
